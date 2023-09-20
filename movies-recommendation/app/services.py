f35b39df4b37616b3bfc7eb74a69a45e6c1d2316
teonakuzmanovska <teona.kuzmanovska@students.finki.ukim.mk>
2023-09-20 15:51:37 +0200

    modified recommendations service

--- movies-recommendation/app/services.py
+++ movies-recommendation/app/services.py
@@ -1,7 +1,7 @@
 from http.client import HTTPException
 import requests
 
-def get_recommendations():
+def get_all_reviews():
     reviews_url = f"http://reviews_service:8001/movies-reviews/reviews"  # reviews_service - name of reviews container
     reviews_response = requests.get(reviews_url, verify=False)
 #     print(movie_response)
@@ -9,18 +9,61 @@ def get_recommendations():
         raise HTTPException(status_code=404, detail="Review not found")
     reviews = reviews_response.json()
 
-    # sort reviews by type
-    sorted_recommendations = sorted(reviews, key=lambda k: k['rating'], reverse=True)
+    return reviews
+
+def group_revies_by_movies():
+    # Create a dictionary to store reviews grouped by movie_id
+    reviews = get_all_reviews()
+    reviews_by_movie = {}
+
+    for review in reviews:
+        movie_id = review["movie_id"]
+        rating = review["rating"]
+        
+        # Check if there's an entry for the movie in reviews_by_movie
+        if movie_id in reviews_by_movie:
+            reviews_by_movie[movie_id].append(rating)
+        else:
+            # Create a new entry with the first rating for the movie
+            reviews_by_movie[movie_id] = [rating]
+
+    # Return dictionary where keys are movie_ids, and values are lists of ratings for each movie.
+    return reviews_by_movie
+
+def calculate_average_ratings():
+    # Create a dictionary to store average ratings for each movie
+    reviews_by_movie = group_revies_by_movies()
+    average_ratings = {}
+
+    for movie_id, ratings in reviews_by_movie.items():
+        if ratings:
+            # Calculate the average rating for the movie
+            average_rating = sum(ratings) / len(ratings)
+        else:
+            # If there are no ratings, set the average rating to 0
+            average_rating = 0
+
+        # Store the average rating in the dictionary
+        average_ratings[movie_id] = average_rating
+
+    return average_ratings
+
+def separate_average_ratings():
+    # Create a list to store separate dictionaries
+    average_ratings = calculate_average_ratings()
+    separate_dicts = []
+
+    for movie_id, average_rating in average_ratings.items():
+        movie_dict = {"movie_id": movie_id, "average_rating": average_rating}
+        separate_dicts.append(movie_dict)
+
+    return separate_dicts
+
+def get_recommendations():
+    # Sort recommendations by ratings and return top 3 recommendations
+    separate_dicts = separate_average_ratings()
+    sorted_recommendations = sorted(separate_dicts, key=lambda x: x["average_rating"], reverse=True)
     top_recommendations = sorted_recommendations[:3]
-    
-    # keep only movie and review
-    top_movies=[]
-    for review in top_recommendations:
-        new_movie = {"movie" : review["movie"], "rating" : review["rating"]}
-        top_movies.append(new_movie)
 
-    # return movie and review
-    return top_movies
+    return top_recommendations
 
-# TODO: grupiraj reviews po movie id, za sekoj movie presmetaj average rating
-# TODO: spored average rating sortiraj i vrati top 3
